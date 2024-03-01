14d2d6a1779948ae82ece4f48c8618a293fe60d4
Christa D. Kitcher <christad.kitcher@Christas-MacBook-Pro.local>
2024-03-01 16:13:54 -0500

    promise final activties

--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -13,19 +13,34 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
 
-  alice1
-    .animate(aliceTumbling, aliceTiming)
-    .finished
-    .then((res) => {
-        console.log(res);
-        alice2
-            .animate(aliceTumbling, aliceTiming)
-            .finished
-            .then((res) => {
-                console.log(res);
-                alice3.animate(aliceTumbling, aliceTiming);
-            })
-    });
+  // alice1
+  //   .animate(aliceTumbling, aliceTiming)
+  //   .finished
+  //   .then((res) => {
+  //       console.log(res);
+  //       alice2
+  //           .animate(aliceTumbling, aliceTiming)
+  //           .finished
+  //           .then((res) => {
+  //               console.log(res);
+  //               alice3.animate(aliceTumbling, aliceTiming);
+  //           })
+  //   });
+
+
+    async function aliceAnimate() {
+      try {
+        firstAnimation = await alice1.animate(aliceTumbling, aliceTiming).finished;
+        secondAnimation =  await alice2.animate(aliceTumbling, aliceTiming).finished;
+        thirdAnimation = await alice3.animate(aliceTumbling, aliceTiming).finished;
+      } catch (error) {
+        console.log(`Error Msg: ${error}`);
+        return 'failed';
+    }
+    
+    }
+
+    aliceAnimate();
 
   // Promise chain  
   // alice1.animate(aliceTumbling, aliceTiming).finished  
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -17,25 +17,45 @@ function sumRow(arr, idx) {
     });
 }
 
-const array2D = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
+async function calculateSum() {
+    const array2D = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
 
-rowSumPromises = [];
+    rowSumPromises = [];
 
-for (let x = 0; x < array2D.length; x++) {
-    rowSumPromises.push(sumRow(array2D[x], x));
+    for (let x = 0; x < array2D.length; x++) {
+        rowSumPromises.push(sumRow(array2D[x], x));
+    }
+    try {
+        const rowSums = await Promise.all(rowSumPromises);
+        let sum = 0;
+        rowSums.forEach(rowSum => {
+            sum += rowSum;d
+        });
+        console.log(`Sum is ${sum}`);
+        return 'done';
+    } catch (error) {
+        console.log(`Error Msg: ${error}`);
+        return 'failed';
+    }
 }
 
-Promise.all(rowSumPromises)
-    .then((result) => {
-       let sum = 0;
-       for (res of result) {
-            sum += res;
-       }
-       console.log(`Total 2D array sum is ${sum}`);
-    }
-    )
-    .catch(() => {console.log('There was an error excecuting this promise')});
+calculateSum().then((status) => console.log(status));
+
+
+// const sum = 
+// Promise.all(rowSumPromises)
+//     .then((result) => {
+//        let sum = 0;
+//        for (res of result) {
+//             sum += res;
+//        }
+//        console.log(`Total 2D array sum is ${sum}`);
+//     }
+//     )
+//     .catch(() => {console.log('There was an error excecuting this promise')});
+
+
